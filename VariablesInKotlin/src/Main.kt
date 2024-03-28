ea32d7bb7ef186ce64a0c0e845172ea932fcc4c3
Arun Rajendran <crarun911@gmail.com>
2024-03-28 17:05:57 +0000

    Byte,Short,Int,Long program

--- VariablesInKotlin/src/Main.kt
+++ VariablesInKotlin/src/Main.kt
@@ -1,7 +1,24 @@
-fun main(){
-    var name ="Arun"
-    name="Rajendran"
-    var age=30
 
-    println("hello $name!,you are $age years old today")
+fun main(){
+    var maxValueByte:Byte=Byte.MAX_VALUE
+    var minValueByte:Byte=Byte.MIN_VALUE
+    println("maxvalue of maxValueByte is : $maxValueByte")
+    println("maxValue of minValueByte is : $minValueByte")
+
+    var maxValueShort:Short=Short.MAX_VALUE
+    var minValueShort:Short=Short.MIN_VALUE
+    println("maxvalue of maxValueShort is : $maxValueShort")
+    println("maxValue of minValueShort is : $minValueShort")
+
+    var maxValueInteger:Int=Int.MAX_VALUE
+    var minValueInteger:Int=Int.MIN_VALUE
+    println("maxvalue of maxValueInteger is : $maxValueInteger")
+    println("maxValue of minValueInteger is : $minValueInteger")
+
+
+    var maxValueLong:Long=Long.MAX_VALUE
+    var minValueLong:Long=Long.MIN_VALUE
+    println("maxvalue of maxValueShort is : $maxValueLong")
+    println("maxValue of minValueShort is : $minValueLong")
+
 }
