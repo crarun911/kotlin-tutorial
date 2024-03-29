e54abe9f8a6adf1cd22c73d7374fae00e1413e60
Arun Rajendran <crarun911@gmail.com>
2024-03-29 09:59:43 +0000

    Byte,Short,Int,Long program

--- VariablesInKotlin/src/Main.kt
+++ VariablesInKotlin/src/Main.kt
@@ -1,24 +1,35 @@
 
 fun main(){
-    var maxValueByte:Byte=Byte.MAX_VALUE
-    var minValueByte:Byte=Byte.MIN_VALUE
+    val maxValueByte:Byte=Byte.MAX_VALUE
+    val minValueByte:Byte=Byte.MIN_VALUE
     println("maxvalue of maxValueByte is : $maxValueByte")
     println("maxValue of minValueByte is : $minValueByte")
 
-    var maxValueShort:Short=Short.MAX_VALUE
-    var minValueShort:Short=Short.MIN_VALUE
+    val maxValueShort:Short=Short.MAX_VALUE
+    val minValueShort:Short=Short.MIN_VALUE
     println("maxvalue of maxValueShort is : $maxValueShort")
     println("maxValue of minValueShort is : $minValueShort")
 
-    var maxValueInteger:Int=Int.MAX_VALUE
-    var minValueInteger:Int=Int.MIN_VALUE
+    val maxValueInteger:Int=Int.MAX_VALUE
+    val minValueInteger:Int=Int.MIN_VALUE
     println("maxvalue of maxValueInteger is : $maxValueInteger")
     println("maxValue of minValueInteger is : $minValueInteger")
 
 
-    var maxValueLong:Long=Long.MAX_VALUE
-    var minValueLong:Long=Long.MIN_VALUE
+    val maxValueLong:Long=Long.MAX_VALUE
+    val minValueLong:Long=Long.MIN_VALUE
     println("maxvalue of maxValueShort is : $maxValueLong")
     println("maxValue of minValueShort is : $minValueLong")
+    
+    val maxfloat=Float.MAX_VALUE
+    val minFloat=Float.MIN_VALUE
+    println("maxvalue of maxfloat is : $maxfloat")
+    println("maxValue of minFloat is : $minFloat")
 
+    val doubleValue:Double=2.5
+    println("double value is :$doubleValue")
+    val charValue="A"
+    println("char value is :$charValue")
+    val  booleanValue:Boolean=true
+    println("boolean value:$booleanValue")
 }
